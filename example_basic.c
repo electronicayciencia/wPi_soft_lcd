502c80b962588f141898a613358fa66f3b4a8034
electronicayciencia <electronicayciencia@gmail.com>
2018-07-28 18:10:32 +0200

    Basic example of usage.

---
+++ example_basic.c
@@ -0,0 +1,37 @@
+/* Program to test LCD basic usage.
+ *
+ * Electronicayciencia
+ * https://github.com/electronicayciencia/wPi_soft_lcd
+ *
+ * Compile this way:
+ * gcc -lwiringPi -o example_basic example_basic.c soft_lcd.c soft_i2c.c
+ *
+ * Reinoso G.   25/07/2018
+ */
+
+
+#include <stdio.h>
+#include "soft_lcd.h"
+
+int main () {
+
+	/* Create a LCD given SCL, SDA and I2C address */
+	lcd_t *lcd = lcd_create(23, 24, 0x3f);
+
+	if (lcd == NULL) {
+		printf("Cannot set-up LCD.\n");
+		return 1;
+	}
+
+	/* Customize options or leave the defaults and init */
+	lcd_init(lcd);
+
+	/* Print a string */
+	lcd_print(lcd, "Hello World!");
+
+	sleep(5);
+
+	/* Turn off LCD and free the memory */
+	lcd_destroy(lcd);
+}
+
