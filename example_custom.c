bf65b5d513766a8cc645d194b2a6d58c4c38ed06
electronicayciencia <electronicayciencia@gmail.com>
2018-07-28 19:06:31 +0200

    Example to show definition and use of custom characters.

---
+++ example_custom.c
@@ -0,0 +1,48 @@
+/* Program to test LCD custom characters.
+ *
+ * Electronicayciencia
+ * https://github.com/electronicayciencia/wPi_soft_lcd
+ *
+ * Compile this way:
+ * gcc -lwiringPi -o example_custom example_custom.c soft_lcd.c soft_i2c.c
+ *
+ * Reinoso G.
+ */
+
+#include <stdio.h>
+#include <time.h>
+#include "soft_lcd.h"
+
+int main () {
+	char battery[] = {
+		0b01110,
+		0b10001,
+		0b10001,
+		0b10001,
+		0b10001,
+		0b10001,
+		0b10001,
+		0b11111
+	};
+
+
+	lcd_t *lcd = lcd_create(23, 24, 0x3f);
+	if (lcd == NULL) {
+		printf("LCD: Cannot initialize\n");
+		return 1;
+	}
+
+	lcd_init(lcd);
+
+	int i;
+	for (i = 7; i >= 1; i--) {
+		printf("Creating character %d...\n",i);
+		battery[i] = 0b11111;
+		lcd_create_char(lcd, i, battery);
+	}
+
+	lcd_print(lcd, "Energy level:");
+	lcd_pos(lcd, 1,9);
+	lcd_print(lcd, "\01\02\03\04\05\06\07");
+}
+
